d196d7d55066453d3fdf8674a29aa9a3a5e56b04
Tomer Arzuan - Home <tomerarzu@gmail.com>
2023-03-31 17:23:18 +0300

    SuccessFlight # [setting up api]

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import logging
 from flask import Flask
 from flask_restful import Api
 
+from resources import FlightsDataResource
+
 logger = logging.getLogger("success_flights")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 
@@ -11,7 +13,13 @@ app = Flask(__name__)
 api = Api(app)
 
 # initializations
-logger.info("first log")
+api.add_resource(
+    FlightsDataResource,
+    '/flights',
+    resource_class_kwargs={
+        # TODO: add an handler for post and get functions
+    }
+)
 
 # run local
 if __name__ == "__main__":
